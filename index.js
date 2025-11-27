1cafa2893eb474ebcaa0509f31d92dd8d723ba67
Julian1045 <jmunoz89@horizon.csueastbay.edu>
2025-11-27 09:35:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+// acessing table element
 let tabble = document.getElementById('one');
 
+// creating array of tr elements
+ numRows = document.getElementsByTagName('tr');
+
+ 
+ // gathering titles and contents
+  let input_array1 = document.getElementsByClassName('BlogTitle');
+  let input_array2 = document.getElementsByClassName('BlogContent');
+
+  function deleteLastElement(arr) {
+    if (arr.length > 0) {
+        arr.length = arr.length - 1;
+    }
+}
 
 function AddRow(){
     // creating array
@@ -22,63 +36,93 @@ function AddRow(){
     let textBox1 = document.createElement('input');
     textBox1.setAttribute('type', 'text');
     textBox1.setAttribute('placeholder','BlogName');
+    textBox1.setAttribute('class','BlogTitle');
+    
+    // here we should try to create a mechanism that saves the data
+   input_array1[input_array1.length] = textBox1;
 
      let textBox2 = document.createElement('input');
     textBox2.setAttribute('type', 'text');
     textBox2.setAttribute('placeholder','Blog');
+    textBox2.setAttribute('class','BlogContent');
+
+    // here we should try to create a mechanism that saves the data
+    input_array2[input_array2.length] = textBox2;
+
+    
 
 // appending textboxes to columns
     arr[1].appendChild(textBox1);
     arr[2].appendChild(textBox2);
 
+    //saving info
+    SaveInfo();
+
 
 }
 
-console.log();
-
 
 function DeleteRow(){
-    // create array of row elements
-    numRows = document.getElementsByTagName('tr');
-    // make it so you cannot delete the first row
+
     if(numRows.length ==2){
         return 0;
         
 
     }
-        // delete the last row
     else{
         tabble.removeChild(numRows[(numRows.length)-1]);
+        deleteLastElement(input_array1);
+        deleteLastElement(input_array2);
 
     }
+    SaveInfo();
 
 }
+let firstTime = 0;
+
 let table_info = [];
+
 function SaveInfo(){
 
-    let input_array1 = document.getElementsByClassName('BlogTitle');
-    let input_array2 = document.getElementsByClassName('BlogContent');
     arr1 = [];
-    arr2 = [];
     
 
 
 
    //getting values of input elements
     for(let i = 0; i< input_array1.length;i++){
-        arr1.push(input_array1[i].value);
+        arr1.push(
+            {
+            title : input_array1[i].value,
+            content: input_array2[i].value
+
+            
+        })
     }
    
-    for(let i = 0; i < input_array2.length;i++){
-        arr2.push(input_array2[i].value);
-    }
-
-
-    arr3 = [arr1,arr2];
-    table_info = arr3;
-    console.log(arr3);
-    return arr3;
-  
 
     
+    
+    table_info = arr1;
+    console.log(table_info);
+
+
+    fetch('http://localhost:3000/index', {
+method: 'POST',
+headers: {
+'Content-Type': 'application/json'
+},
+body: JSON.stringify(table_info)
+})
+    
 }
+
+
+
+
+
+
+
+
+    
+
